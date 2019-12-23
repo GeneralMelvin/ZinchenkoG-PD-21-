e7bdcdcebba742280c7eb69518edeabb4706c698
GeneralMelvin <58726179+GeneralMelvin@users.noreply.github.com>
2019-12-23 21:28:08 +0200

    Add files via upload

---
+++ Lab8/Lab8/Garage.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Car
+    {
+        public string Name { get; }
+        public string Color { get; }
+        public int Speed { get; }
+        public int YearOfIssue { get; }
+
+        public Car() { }
+        public Car(string name, string color, int speed, int yearOfIssue)
+        {
+            Name = name;
+            Color = color;
+            Speed = speed;
+            YearOfIssue = yearOfIssue;
+        }
+    }
+    class Garage
+    {
+        public List<Car> myCars = new List<Car>();
+
+        public void DeleteCar()
+        {
+            int position;
+            Console.Write("Виберіть машину, яку необхідно прибрати: ");
+            while (!int.TryParse(Console.ReadLine(), out position) || position < 1 || position > myCars.Count)
+            {
+                Console.Write("Спробуйте інший номер: ");
+            }
+            myCars.RemoveAt(position - 1);
+            Console.WriteLine("Машину під номером {0} було прибрано", position);
+        }
+        public void Addition()
+        {
+            string name;
+            string color;
+            int speed;
+            int year;
+
+            Console.Write("\nВведіть назву машини: ");
+            name = Console.ReadLine();
+            Console.Write("Введіть колір машини: ");
+            color = Console.ReadLine();
+            Console.Write("Введіть рік виходу машини: ");
+            while (!int.TryParse(Console.ReadLine(), out year) || year > 2020 || year < 1850)
+            {
+                Console.Write("Введіть корректний рік (1850< year < 2020: ");
+            }
+            Console.Write("Введіть швидкість машини: ");
+            while (!int.TryParse(Console.ReadLine(), out speed) || speed <= 0)
+            {
+                Console.Write("Введіть коректне значення швидкості(Більше 0): ");
+            }
+
+
+
+            Car newCar = new Car(name, color, speed, year);
+
+            myCars.Add(newCar);
+        }
+
+        public void InfoAboutCar(Car car)
+        {
+            Console.WriteLine("\nName  : {0}\nColor  : {1}\nSpeed  : {2}\nYear  : {3}", car.Name, car.Color, car.Speed, car.YearOfIssue);
+        }
+
+        public void AllCars()
+        {
+            Console.WriteLine("\n Усі машини, що в гаражі:");
+            int i = 1;
+            foreach (Car c in myCars)
+            {
+                InfoAboutCar(c);
+                i++;
+            }
+        }
+
+        public void CharacteristicSearch(char characteristic)
+        {
+            int i = 0;
+            switch (characteristic)
+            {
+                case 'n':
+                    Console.Write("За якою назвою ви бажаєте знайти: ");
+                    string name = Console.ReadLine();
+
+                    foreach (Car c in myCars)
+                    {
+                        if (c.Name == name)
+                        {
+                            InfoAboutCar(c);
+                            i++;
+                        }
+                    }
+                    if (i == 0) Console.WriteLine("Помилка! Такої назви не знайдено");
+                    { i = 0; }
+                    break;
+                case 'c':
+                    Console.Write("За яким кольором ви бажаєте шукати: ");
+                    string color = Console.ReadLine();
+                    foreach (Car c in myCars)
+                    {
+                        if (c.Color == color)
+                        {
+                            InfoAboutCar(c);
+                            i++;
+                        }
+                    }
+                    if (i == 0) Console.WriteLine("Помилка! Такого кольору не знайдено");
+                    i = 0;
+                    break;
+                case 's':
+                    Console.Write("Яка швидкість шуканої машини: ");
+                    int speed = Convert.ToInt32(Console.ReadLine());
+                    foreach (Car c in myCars)
+                    {
+                        if (c.Speed == speed)
+                        {
+                            InfoAboutCar(c);
+                            i++;
+                        }
+                    }
+                    if (i == 0) Console.WriteLine("Помилка! Машин з такою швидкістю не знайдено");
+                    i = 0;
+                    break;
+                case 'y':
+                    Console.Write("Якого року випуску: ");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    foreach (Car c in myCars)
+                    {
+                        if (c.YearOfIssue == year)
+                        {
+                            InfoAboutCar(c);
+                            i++;
+                        }
+                    }
+                    if (i == 0) Console.WriteLine("Помилка! Машин цього року не знайдено");
+                    i = 0;
+                    break;
+                default:
+                    Console.WriteLine("Помилка! Даної опції не знайдено");
+                    break;
+            }
+
+        }
+
+
+    }
+}
---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage myGarage = new Garage();
+            Console.WriteLine("Виберіть машину:");
+            int choice;
+            do
+            {
+                Console.WriteLine();
+                if (myGarage.myCars.Count == 0)
+                {
+                    Console.WriteLine("Введіть 0, аби залишити гараж");
+                    Console.WriteLine("Введіть 1, аби поставити нову машину в гараж");
+                }
+                else
+                {
+                    Console.WriteLine("Введіть 0, аби залишити гараж");
+                    Console.WriteLine("Введіть 1, аби поставити нову машину в гараж");
+                    Console.WriteLine("Введіть 2, аби побачити усі машини");
+                    Console.WriteLine("Введіть 3, аби прибрати машину з гаражу");
+                    Console.WriteLine("Введіть 4, аби вибрати машину за критеріями");
+
+                }
+                Console.Write("Введіть запит:");
+                while (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Введіть корректний запит: ");
+                }
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Гараз зачинено.");
+                        return;
+                    case 1:
+                        Console.WriteLine("Машину поставлено");
+                        myGarage.Addition();
+                        Console.WriteLine(myGarage.myCars.Count);
+                        break;
+                    case 2:
+                        myGarage.AllCars();
+                        break;
+                    case 3:
+                        myGarage.DeleteCar();
+                        break;
+                    case 4:
+                        Console.WriteLine("За якою характеристикою шукаємо");
+                        char characteristic;
+                        Console.WriteLine("n - Ім'я");
+                        Console.WriteLine("c - Колір");
+                        Console.WriteLine("s - Швидкість");
+                        Console.WriteLine("y - Рік випуску");
+                        characteristic = Convert.ToChar(Console.ReadLine());
+                        myGarage.CharacteristicSearch(characteristic);
+                        break;
+
+                    default:
+                        Console.WriteLine("Помилка! Введіть щось з наведеного");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
