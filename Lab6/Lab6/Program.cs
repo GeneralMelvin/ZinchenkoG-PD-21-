0860d5ca3f1e228368bee4e88d9e1e3947c8be0f
GeneralMelvin <58726179+GeneralMelvin@users.noreply.github.com>
2019-12-23 21:03:27 +0200

    Add files via upload

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int[] count = new int[2];
+
+            List<double> numbers = new List<double>() { };
+
+
+            Console.WriteLine("Введіть розмір");
+
+
+            int.TryParse(Console.ReadLine(), out int n);
+            for (int i = 0; i < n; i++)
+            {
+                do
+                {
+
+                    int.TryParse(Console.ReadLine(), out a);
+
+                } while (a != 0 && a != 1);
+
+                numbers.Add(a);
+                count[a]++;
+            }
+
+            double[] arr = numbers.ToArray();
+
+            foreach (int i in arr) 
+            {
+                Console.Write("{0} ", i);
+            }
+
+            Console.WriteLine("\nКількість 0 та 1");
+
+            foreach (int i in count) 
+            {
+                Console.Write("{0} ", i);
+            }
+
+        }
+    }
+}
