a8fbc35d31e5590d244dcdebf95c31ae9ffd044d
GeneralMelvin <58726179+GeneralMelvin@users.noreply.github.com>
2020-02-26 17:19:06 +0200

    Add files via upload

---
+++ Щютка/Щютка/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Щютка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Abaddon Cyr", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(460, 181);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ДА";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 301);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Жми на кнопочку \"ДА\", чтобы получить Dragon Lore!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Щютка/Щютка/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Щютка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += Form1_MouseMove;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.button1.Location.X < e.X && this.button1.Location.Y - 25 < e.Y && this.button1.Location.Y > e.Y && this.button1.Location.X + button1.Width > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X, this.button1.Location.Y + 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.X + button1.Width < e.X && this.button1.Location.X + button1.Width + 25 > e.X && this.button1.Location.Y - 25 < e.Y && this.button1.Location.Y > e.Y)
+            {
+                this.button1.Location = new Point(this.button1.Location.X - 10, this.button1.Location.Y + 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.Y < e.Y && this.button1.Location.X + button1.Width < e.X && this.button1.Location.X + button1.Width + 25 > e.X && this.button1.Location.Y + button1.Height > e.Y)
+            {
+                this.button1.Location = new Point(this.button1.Location.X - 10, this.button1.Location.Y);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.Y + button1.Height < e.Y && this.button1.Location.Y + button1.Height + 25 > e.Y && this.button1.Location.X + button1.Width < e.X && this.button1.Location.X + button1.Width + 25 > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X - 10, this.button1.Location.Y - 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.X < e.X && this.button1.Location.Y + button1.Height + 25 > e.Y && this.button1.Location.Y + button1.Height < e.Y && this.button1.Location.X + button1.Width > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X, this.button1.Location.Y - 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.X - 25 < e.X && this.button1.Location.Y + button1.Height + 25 > e.Y && this.button1.Location.Y + button1.Height < e.Y && this.button1.Location.X > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X + 10, this.button1.Location.Y - 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.X - 25 < e.X && this.button1.Location.Y + button1.Height > e.Y && this.button1.Location.Y < e.Y && this.button1.Location.X > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X + 10, this.button1.Location.Y);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+            if (this.button1.Location.X - 25 < e.X && this.button1.Location.Y - 25 < e.Y && this.button1.Location.Y > e.Y && this.button1.Location.X > e.X)
+            {
+                this.button1.Location = new Point(this.button1.Location.X + 10, this.button1.Location.Y + 10);
+                this.button1.Size = new Size(this.button1.Size.Width, this.button1.Size.Height - 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
